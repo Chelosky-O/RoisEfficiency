54754c6157c8fbf54a1433f1b60888f7cdaf3541
unknown <icaceressatorres2001@gmail.com>
2023-05-05 17:03:51 -0400

    agregando carrito de compras

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var ejs = require('ejs');
 var bodyParser = require('body-parser');
 var mysql = require('mysql');
+var session = require('express-session');
 
 mysql.createConnection({
     host:"localhost",
@@ -18,6 +19,35 @@ app.set('view engine', 'ejs');
 
 app.listen(8080);
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(session({
+    secret: 'mysecret',
+    resave: false,
+    saveUninitialized: false
+  }));
+  
+
+function isProductInCart(cart, id){
+    for(let i=0; i<cart.length; i++){
+        if(cart[i].id == id){
+            return true;
+        }
+    }
+    return false;
+}
+
+function calculateTotal(cart,req){
+    total = 0;
+    for(let i=0; i<cart.length; i++){
+        if(cart[i].sale_price){
+            total = total + (cart[i].sale_price * cart[i].quantity);
+        }else{
+            total = total + (cart[i].price * cart[i].quantity);
+        }
+    }
+    req.session.total = total;
+    return total;
+}
+
 
 //Acceso index
 app.get('/', function(req,res){
@@ -45,5 +75,53 @@ app.get('/login', function(req,res){
 app.get('/producto', function(req,res){
     res.render('pages/producto');
 });
+
+app.post('/add_to_cart', function(req,res){
+    var id = req.body.id;
+    var name = req.body.name;
+    var price = req.body.price;
+    var sale_price = req.body.sale_price;
+    var quantity = req.body.quantity;
+    var image = req.body.image;
+    var product =  {id:id,name:name,price:price,sale_price:sale_price,quantity:quantity,image:image};
+
+    if(req.session.cart){
+        var cart = req.session.cart;
+
+        if(!isProductInCart(cart, id)){
+            cart.push(product);
+        }
+    }
+    else{
+        req.session.cart = [product];
+        var cart = req.session.cart;
+    }
+
+    calculateTotal(cart,req);
+    res.redirect('/cart');
+
+});
+
+app.get('/cart', function(req,res){
+    var cart = req.session.cart;
+    var total = req.session.total;
+
+    res.render('pages/carrito',{cart:cart,total:total});
+});
+
+app.post('/remove_product', function(req,res){
+    var id = req.body.id;
+    var cart = req.session.cart;
+    
+    for(let i=0; i<cart.length; i++){
+        if(cart[i].id == id){
+            cart.splice(cart.indexOf(i),1);
+        }
+    }
+
+    calculateTotal(cart,req);
+    res.redirect('/cart');
+});
+
 
 app.use( express.static( "views" ) );
