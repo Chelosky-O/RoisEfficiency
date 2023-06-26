b0bb16237850cbdd8dce138a26bb14e004ca89ee
tobias <tobias.guerrero_c@mail.udp.cl>
2023-06-25 21:54:08 -0400

    barra busqueda

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -6,7 +6,19 @@
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
 
-document.addEventListener("keyup", e=>{
-    e.target.matches("buscador")
-    console.log(e.target.value())
-} )
+//BUscador
+//ejecutandose
+document.getElementById("icon-search").addEventListener("click", mostrar_buscador);
+
+
+//Variables a utilizar
+bars_search = document.getElementById("ctn-bars-search");
+cover_ctn_search = document.getElementById("cover-ctn-search");
+inputSearch = document.getElementById("inputSearch");
+box_search =document.getElementById("box-search");
+
+
+//Funcion mostrar el buscador
+function mostrar_buscador(){
+    bars_search.style.top = "80px"
+}
