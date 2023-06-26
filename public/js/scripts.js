2d019be48c3cbaa4bf1e4aaa80b2a194eb2e678d
tobias <tobias.guerrero_c@mail.udp.cl>
2023-06-26 01:10:35 -0400

    barra de busqueda casi lista

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -9,6 +9,7 @@
 //BUscador
 //ejecutandose
 document.getElementById("icon-search").addEventListener("click", mostrar_buscador);
+document.getElementById("cover-ctn-search").addEventListener("click", ocultar_buscador);
 
 
 //Variables a utilizar
@@ -21,4 +22,48 @@ box_search =document.getElementById("box-search");
 //Funcion mostrar el buscador
 function mostrar_buscador(){
     bars_search.style.top = "80px"
+    cover_ctn_search.style.display = "block";
+    inputSearch.focus();
+
+    if(inputSearch.value === ""){
+        box_search.style.display = "none";
+    }
+}
+
+
+
+//funcion para ocultar buscado
+function ocultar_buscador(){
+    bars_search.style.top = "-10px";
+    cover_ctn_search.style.display = "none";
+    inputSearch.value = "";
+    box_search.style.display = "none";
+
+}
+
+//Creando filtrado de busqueda
+
+document.getElementById("inputSearch").addEventListener("keyup", buscador_interno);
+
+function buscador_interno(){
+    filter = inputSearch.value.toUpperCase();
+    li = box_search.getElementsByTagName("li");
+
+    //Recorriendo elementos a filtrar mediantes los li
+    for(i = 0; i < li.length; i++){
+        a = li[i].getElementsByTagName("a")[0];
+        textValue = a.textContent || a.innerText;
+
+        if(textValue.toUpperCase().indexOf(filter) > -1){
+            li[i].style.display = "";
+            box_search.style.display = "block";
+
+            if(inputSearch.value === ""){
+                box_search.style.display = "none";
+            }
+
+        }else{
+            li[i].style.display = "none";
+        }
+    }
 }
